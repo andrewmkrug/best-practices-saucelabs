88401895108b0340b913616ed65e84d78286c17f
andrewkrug <andrew.krug@saucelabs.com>
2019-08-27 02:53:22 -0500

    init for exercise 6 pages

--- best-practices/Pages/ConfirmationPage.cs
+++ best-practices/Pages/ConfirmationPage.cs
@@ -3,17 +3,31 @@ using OpenQA.Selenium;
 
 namespace Tests.Pages
 {
-    public class ConfirmationPage
+    public class ConfirmationPage : BasePage
     {
-        private IWebDriver driver;
-
         public ConfirmationPage(IWebDriver driver) {
-            this.driver = driver;
+            super(driver);
+        }
+        public void visit()
+        {
+            driver.navigate().to("https://www.saucedemo.com/checkout-step-two.html");
         }
 
-        public CheckoutCompletePage finish() {
-            String finished =".btn_action.cart_button";
-            driver.FindElement(By.CssSelector(finished)).Click();
+        public void setPageState()
+        {
+            ((JavascriptExecutor)driver).executeScript("window.sessionStorage.setItem('standard-username', 'standard-user')");
+            ((JavascriptExecutor)driver).executeScript("window.sessionStorage.setItem('cart-contents', '[4,1]')");
+            driver.navigate().refresh();
+        }
+        public Boolean hasItems() {
+            String cartBadge = "shopping_cart_badge";
+            return Integer.parseInt(driver.findElement(By.className(cartBadge)).getText()) > 0;
+        }
+        public CheckoutCompletePage FinishCheckout()
+        {
+            String finished = ".btn_action.cart_button";
+            WebElement finishButton = driver.findElement(By.cssSelector(finished));
+            finishButton.click();
             return new CheckoutCompletePage(driver);
         }
     }
